f245a595cf7776081e2b24115b2a58ec6176e479
pc-master-user <prabhakarchowdary777@gmail.com>
2021-04-01 16:59:26 +0530

    first commit

---
+++ example/android/app/src/main/java/com/clinix/call_service_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.clinix.call_service_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/CallServicePlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface CallServicePlugin : NSObject<FlutterPlugin>
+@end
